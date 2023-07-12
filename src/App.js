b72bb53397869ed82ddee94b0405773702106e60
leekj9633 <leekj9633@sookmyung.ac.kr>
2023-07-12 14:54:21 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link" //aljfldkfjs
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
